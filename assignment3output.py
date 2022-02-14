31482d1a31d4488b008fb25ca62d8befc9553861
deepankshu-123 <97288003+deepankshu-123@users.noreply.github.com>
2022-02-14 23:10:25 +0530

    Add files via upload

---
+++ assignment3output.py
@@ -0,0 +1,49 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\DELL\Desktop\assignment 3.py ================
+enter a word or sentence deepankshu
+{'d': 1, 'e': 2, 'p': 1, 'a': 1, 'n': 1, 'k': 1, 's': 1, 'h': 1, 'u': 1}
+enter day 4
+enter month 5
+enter year 2006
+Next Date is: 5 / 5 / 2006
+enter no. of elements3
+element4
+element5
+element6
+[(4, 16), (5, 25), (6, 36)]
+enter grade point 8
+Your Grade is 'B+' and Very Good performance
+A B C D E F G H I J K 
+  A B C D E F G H I 
+    A B C D E F G 
+      A B C D E 
+        A B C 
+          A 
+enter sid 21104022
+enter name deepak
+Y or N Y
+enter sid 21104013
+enter name priya
+Y or N Y
+enter sid 21104014
+enter name raj
+Y or N N
+{21104022: 'deepak', 21104013: 'priya', 21104014: 'raj'}
+[(21104022, 'deepak'), (21104013, 'priya'), (21104014, 'raj')]
+(21104013, 'priya') (21104014, 'raj') (21104022, 'deepak') 
+
+enter sid that you want to search  21104013
+priya
+enter no. of terms5
+Fibonacci sequence:
+01123
+
+average of series is  1.4
+{1, 3, 5, 6, 8}
+{3, 6, 8, 9, 13, 17}
+{1, 2, 4, 5}
+{6, 7, 8, 9, 10}
+{10, 7}
+
